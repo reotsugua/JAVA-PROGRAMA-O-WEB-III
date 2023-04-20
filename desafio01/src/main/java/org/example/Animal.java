fad34671b413416db55e40b4ad3c7c18ce9926ce
reotsugua <renanaugusto.ras@gmail.com>
2023-04-20 18:46:39 -0300

    Cenarios (Então, elabore de 3 a 5 cenários de uso utilizando operadores da Stream API para selecionar animais em condições que você mesmo irá definir.)

--- desafio01/src/main/java/org/example/Animal.java
+++ desafio01/src/main/java/org/example/Animal.java
@@ -4,23 +4,56 @@ public class Animal {
     private String alimentacao;
     private String reproducao;
     private String locomocao;
+    private double peso;
+    private double tamanho;
 
-    public Animal(String alimentacao, String reproducao, String locomocao) {
+    public Animal(String alimentacao, String reproducao, String locomocao, double peso, double tamanho) {
         this.alimentacao = alimentacao;
         this.reproducao = reproducao;
         this.locomocao = locomocao;
+        this.peso = peso;
+        this.tamanho = tamanho;
     }
 
     public String getAlimentacao() {
         return alimentacao;
     }
 
+    public void setAlimentacao(String alimentacao) {
+        this.alimentacao = alimentacao;
+    }
+
     public String getReproducao() {
         return reproducao;
+    }
+
+    public void setReproducao(String reproducao) {
+        this.reproducao = reproducao;
     }
 
     public String getLocomocao() {
         return locomocao;
     }
+
+    public void setLocomocao(String locomocao) {
+        this.locomocao = locomocao;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(double tamanho) {
+        this.tamanho = tamanho;
+    }
 }
+
 
--- desafio01/src/main/java/org/example/Main.java
+++ desafio01/src/main/java/org/example/Main.java
@@ -1,13 +1,46 @@
 package org.example;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
 
-        Animal leao = new Animal("Carnívoro", "Vivíparo", "Terrestre");
-        Animal elefante = new Animal("Herbívoro", "Vivíparo", "Terrestre");
-        Animal tubarao = new Animal("Carnívoro", "Ovíparo", "Aquático");
-        Animal cobra = new Animal("Carnívoro", "Ovíparo", "Terrestre");
-        Animal tartaruga = new Animal("Herbívoro", "Ovíparo", "Terrestre");
+        Animal leao = new Animal("carnívoro", "vivíparo", "terrestre", 180, 2.5);
+        Animal vaca = new Animal("herbívoro", "vivíparo", "terrestre", 500, 1.5);
+        Animal cavalo = new Animal("herbívoro", "vivíparo", "terrestre", 600, 1.7);
+        Animal elefante = new Animal("herbívoro", "vivíparo", "terrestre", 5000, 3.5);
+        Animal pinguim = new Animal("carnívoro", "ovíparo", "aquático", 10, 0.6);
+        Animal tucano = new Animal("carnívoro", "ovíparo", "aéreo", 0.5, 0.3);
+        Animal crocodilo = new Animal("carnívoro", "ovíparo", "aquático", 700, 3.2);
+        Animal tartaruga = new Animal("herbívoro", "ovíparo", "aquático", 50, 0.8);
+
+
+
+        List<Animal> animais = Arrays.asList(leao, vaca, cavalo, elefante, pinguim, tucano, crocodilo, tartaruga);
+        double pesoMinimo = 200;
+
+        List<Animal> herbivorosPesados = animais.stream()
+                .filter(a -> a.getAlimentacao().equals("herbívoro"))
+                .filter(a -> a.getPeso() > pesoMinimo)
+                .collect(Collectors.toList());
+
+        List<Animal> oviparosNaoAereos = animais.stream()
+                .filter(a -> a.getReproducao().equals("ovíparo"))
+                .filter(a -> !a.getLocomocao().equals("aéreo"))
+                .collect(Collectors.toList());
+
+
+
+        Optional<Animal> animalMaisPesado = animais.stream()
+                .filter(a -> a.getAlimentacao().equals("herbívoro"))
+                .max(Comparator.comparingDouble(Animal::getPeso));
+
+
+
+
+
+
 
     }
 }
