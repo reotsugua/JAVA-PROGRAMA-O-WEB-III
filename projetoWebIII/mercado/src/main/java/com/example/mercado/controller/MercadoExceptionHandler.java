7d0a3c5d6422382e83c174d922947628900122d3
reotsugua <renanaugusto.ras@gmail.com>
2023-04-24 20:14:22 -0300

    Exception Handler pronto

--- projetoWebIII/mercado/src/main/java/com/example/mercado/controller/MercadoExceptionHandler.java
+++ projetoWebIII/mercado/src/main/java/com/example/mercado/controller/MercadoExceptionHandler.java
@@ -1,4 +1,30 @@
 package com.example.mercado.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
 public class MercadoExceptionHandler {
+
+    @ExceptionHandler(value = {Exception.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<String> handleException(Exception e) {
+        log.error("Unexpected error", e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Unexpected error: " + e.getMessage());
+    }
+
+    @ExceptionHandler(value = {RuntimeException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException e) {
+        log.error("Runtime error", e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("Runtime error: " + e.getMessage());
+    }
 }
+
