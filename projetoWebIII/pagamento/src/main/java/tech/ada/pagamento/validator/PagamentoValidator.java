a1cb73ed7d1ebd3eeba62037a24dedef85dd9896
reotsugua <renanaugusto.ras@gmail.com>
2023-04-24 21:21:24 -0300

    validador de pagamento

--- projetoWebIII/pagamento/src/main/java/tech/ada/pagamento/validator/PagamentoValidator.java
+++ projetoWebIII/pagamento/src/main/java/tech/ada/pagamento/validator/PagamentoValidator.java
@@ -2,19 +2,24 @@ package tech.ada.pagamento.validator;
 
 import org.springframework.stereotype.Component;
 import tech.ada.pagamento.model.Pagamento;
-import tech.ada.pagamento.model.Usuario;
-
-import java.util.Map;
+import tech.ada.pagamento.model.SaldoInsuficienteException;
+import tech.ada.pagamento.model.UsuarioSaldo;
 
 @Component
 public class PagamentoValidator {
 
-    private Map<String, Double> saldoUsuario;
-
     public void validarSaldo(Pagamento pagamento) {
-        Double saldoPagador = saldoUsuario.get(pagamento.getPagador());
-        if (saldoPagador == null || saldoPagador < pagamento.getValor()) {
-            throw new IllegalArgumentException("Saldo insuficiente para realizar o pagamento");
+
+        UsuarioSaldo usuarioSaldo = buscarSaldo(pagamento.getPagador());
+
+        if (usuarioSaldo.getSaldo() < pagamento.getValor()) {
+            throw new SaldoInsuficienteException("Saldo insuficiente para realizar o pagamento");
         }
     }
+
+    private UsuarioSaldo buscarSaldo(String usuarioId) {
+
+        return new UsuarioSaldo(usuarioId, "Usuário " + usuarioId, 1000.0);
+    }
+
 }
